799e3f9189408a69b3f96f511c97a29e77515a7d
felixkariuki3 <felka39@gmail.com>
2025-07-31 12:49:59 +0300

    WIP Fixes

--- backend/models/production_order.py
+++ backend/models/production_order.py
@@ -39,14 +39,3 @@ class ProductionOperation(Base):
 
     order = relationship("ProductionOrder", back_populates="operations")
     
-class WorkInProgress(Base):
-    __tablename__ = "work_in_progress"
-
-    id = Column(Integer, primary_key=True, index=True)
-    production_order_id = Column(Integer, ForeignKey("production_orders.id"))
-    issued_quantity = Column(Float, default=0)
-    completed_quantity = Column(Float, default=0)
-    status = Column(String, default="in_progress")
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
-
-    production_order = relationship("ProductionOrder", back_populates="wip")
