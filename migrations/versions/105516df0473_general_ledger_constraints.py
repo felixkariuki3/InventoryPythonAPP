00fee174a9a6b4930568fbdc9a7a4b2c13308cc5
felixkariuki3 <felka39@gmail.com>
2025-10-30 10:40:44 +0300

    Updates

--- migrations/versions/105516df0473_general_ledger_constraints.py
+++ migrations/versions/105516df0473_general_ledger_constraints.py
@@ -61,9 +61,8 @@ def upgrade() -> None:
                type_=sa.Numeric(precision=18, scale=2),
                existing_nullable=True)
         batch_op.drop_index(batch_op.f('ix_journal_lines_account'))
-        batch_op.drop_constraint(None, type_='foreignkey')
         batch_op.create_foreign_key('fk_journal_lines_entry_id', 'journal_entries', ['entry_id'], ['id'])
-        batch_op.create_foreign_key(None, 'accounts', ['account_id'], ['code'])
+        batch_op.create_foreign_key('fk_journal_lines_account_id', 'accounts', ['account_id'], ['code'])
         batch_op.drop_column('journal_id')
         batch_op.drop_column('account')
 
@@ -76,9 +75,8 @@ def downgrade() -> None:
     with op.batch_alter_table('journal_lines', schema=None) as batch_op:
         batch_op.add_column(sa.Column('account', sa.VARCHAR(), nullable=True))
         batch_op.add_column(sa.Column('journal_id', sa.INTEGER(), nullable=True))
-        batch_op.drop_constraint(None, type_='foreignkey')
         batch_op.drop_constraint('fk_journal_lines_entry_id', type_='foreignkey')
-        batch_op.create_foreign_key(None, 'journal_entries', ['journal_id'], ['id'])
+        batch_op.create_foreign_key('fk_journal_lines_journal_id', 'journal_entries', ['journal_id'], ['id'])
         batch_op.create_index(batch_op.f('ix_journal_lines_account'), ['account'], unique=False)
         batch_op.alter_column('credit',
                existing_type=sa.Numeric(precision=18, scale=2),
